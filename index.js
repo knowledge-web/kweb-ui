401e49d5be2d50fcf2fd7426cd2d9e9489f6ee3e
Karl Svartholm <gorbiz@gmail.com>
2023-07-18 11:49:14 +0200

    fix some urls & add a landing page

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ const port = process.env.PORT || 7575
 const config = { }
 config.hideBrainId = process.env.HIDE_BRAINID === 'false' ? false : true
 config.hidePrivate = process.env.HIDE_PRIVATE === 'false' ? false : true
-// config.hideWip = process.env.HIDE_WIP === 'false' ? false : true
-config.rootNode = process.env.ROOT_NODE || '335994d7-2aff-564c-9c20-d2c362e82f8c'
+config.hideWip = process.env.HIDE_WIP === 'false' ? false : true
+config.rootNode = process.env.ROOT_NODE || '335994d7-2aff-564c-9c20-d2c362e82f8c' // "Knowledge Web" node
 
 let fileCache = {} // { filename, mtimeMs, data }
 function loadJson (name) {
@@ -62,9 +62,22 @@ function getLinks () {
 
 const app = express()
 
+app.get('/', (req, res) => {
+  res.send(`<h1>Welcome to the Knowledge Web prototype!</h1>
+  <p>
+    This is what we are building: <a href="https://burkives.notion.site/K-Web-UI-Minimal-Product-dc76722ef10b465c8d987e8f16bedc95?pvs=4">Design document</a>.<br>
+    This is where it is built (starting 2023-07-18): <a href="/ui">prototype</a>.<br>
+    <br>
+    Join the community on <a href="https://www.facebook.com/James-Burkes-Knowledge-Web-1636642569892667/">Facebook</a>.<br>
+    Want to work on the code? Contact karl@passionismandatory.com.
+  </p>`)
+})
+
 app.use('/ui', express.static('./ui'))
 
-app.get('/nodes/:id?', (req, res) => {
+// TODO move to separate file
+const api = express.Router()
+api.get('/nodes/:id?', (req, res) => {
   const id = req.params.id || config.rootNode
   // const levels = req.query.levels || 1 // TODO
   const node = getNodes().find(n => n.Id === id)
@@ -101,6 +114,8 @@ app.get('/nodes/:id?', (req, res) => {
 
 getNodes()
 getLinks()
+
+app.use('/api/v0', api)
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
