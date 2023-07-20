e9b2078b997599acdf160275678bcef7e91b23e5
Karl Svartholm <gorbiz@gmail.com>
2023-07-20 11:14:47 +0200

    spit out api & disable CORS

--- index.js
+++ index.js
@@ -1,72 +1,6 @@
 const express = require('express')
-const fs = require('fs')
-const path = require('path')
-
-const brainDir = process.env.BRAIN_DIR || '../Brain/B02'
-const brainJsonDir = process.env.BRAIN_JSON_DIR ? process.env.BRAIN_JSON_DIR : path.join(brainDir, '../db') // ex '../Brain/db'
+const api = require('./api')
 const port = process.env.PORT || 7575
-
-const rootNode = process.env.ROOT_NODE || '335994d7-2aff-564c-9c20-d2c362e82f8c' // "Knowledge Web" node
-
-function mapFrom (list, { key } = { key: 'Id' }) {
-  const map = {}
-  list.forEach(item => map[item[key]] = item)
-  return map
-}
-
-function readFile (file) {
-  if (!fs.existsSync(file)) return ''
-  return fs.readFileSync(file, 'utf8')
-}
-
-function getContent (id) {
-  const f = path.join(brainDir, id)
-  if (!fs.existsSync(f) || !fs.statSync(f).isDirectory()) return
-  if (!/^[-0-9a-f]{36}$/.test(id)) return
-  return {
-    md: readFile(path.join(f, 'Notes.md')),
-    html: readFile(path.join(f, 'Notes/notes.html'))
-  }
-}
-
-let fileCache = {} // { filename, mtimeMs, data }
-function loadJson (name) {
-  const filename = path.join(brainJsonDir, name)
-  const stats = fs.statSync(filename)
-  if (fileCache[name] && stats.mtimeMs === fileCache[name].mtimeMs) return fileCache[name].data
-
-  console.log(`reloading ${name}`)
-  const raw = fs.readFileSync(filename)
-  let data = JSON.parse(raw)
-
-  if (name === 'thoughts.json') {
-    data = data.filter(t => t.ForgottenDateTime === null) // exclude removed
-  } else if (name === 'links.json') {
-    const nodes = loadJson('thoughts.json')
-    const map = mapFrom(nodes)
-    data = data.filter(l => map[l.ThoughtIdA] && map[l.ThoughtIdB]) // exclude links not linking to available thoughts
-    // console.log(data.length, '<-- links')
-  }
-
-  if (!fileCache[name]) fileCache[name] = {}
-  fileCache[name].data = data
-  fileCache[name].mtimeMs = stats.mtimeMs
-
-  return data
-}
-
-function getNodes () {
-  return loadJson('thoughts.json')
-}
-function getNodesMap () {
-  const nodes = getNodes()
-  return mapFrom(nodes)
-}
-
-function getLinks () {
-  const nodes = getNodes()
-  return loadJson('links.json')
-}
 
 const app = express()
 
@@ -82,47 +16,6 @@ app.get('/info', (req, res) => {
 })
 
 app.use('/', express.static('./ui'))
-
-// TODO move to separate file
-const api = express.Router()
-api.get('/nodes/:id?', (req, res) => {
-  const id = req.params.id || rootNode
-  // const levels = req.query.levels || 1 // TODO
-  const node = getNodes().find(n => n.Id === id)
-  if (!node) return res.send({ })
-
-  let links = getLinks().filter(l => l.ThoughtIdA === id || l.ThoughtIdB === id)
-  const map = getNodesMap()
-
-  links = links.map(l => {
-    return {
-      from: l.ThoughtIdA,
-      to: l.ThoughtIdB,
-    }
-  })
-
-  let nodes = {}
-  nodes[id] = node
-
-  links.forEach(l => {
-    nodes[l.from] = map[l.from]
-    nodes[l.to] = map[l.to]
-  })
-
-  nodes = Object.values(nodes)
-  nodes = nodes.map(n => {
-    return {
-      id: n.Id,
-      label: n.Name
-    }
-  })
-
-  const content = getContent(id) || { md: '', html: '' }
-  res.send({ nodes, links, content })
-})
-
-getNodes()
-getLinks()
 
 app.use('/api/v0', api)
 
--- ui/README.md
+++ ui/README.md
@@ -1,1 +1,13 @@
-TODO Make this its own (public) repository!
+# This is the User Interface part
+...the most important bit.
+
+You should (knowing a bit on front-end / Javascript coding) be able to use this separately.
+And talk to the ~official API (https://k-web.ismandatory.com/api/v0).
+
+Try this:
+
+```sh
+cd ui
+npm install --global http-server
+http-server .
+```
