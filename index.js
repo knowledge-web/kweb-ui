ee72cf1d7eef4318a8da4a94e49a2706885c4e15
Karl Svartholm <gorbiz@gmail.com>
2023-07-20 10:15:00 +0200

    land on prototype; link to info page

--- index.js
+++ index.js
@@ -70,18 +70,18 @@ function getLinks () {
 
 const app = express()
 
-app.get('/', (req, res) => {
+app.get('/info', (req, res) => {
   res.send(`<h1>Welcome to the Knowledge Web prototype!</h1>
   <p>
     This is what we are building: <a href="https://burkives.notion.site/K-Web-UI-Minimal-Product-dc76722ef10b465c8d987e8f16bedc95?pvs=4">Design document</a>.<br>
-    This is where it is built (starting 2023-07-18): <a href="/ui">prototype</a>.<br>
+    This is where it is built (starting 2023-07-18): <a href="/">prototype</a>.<br>
     <br>
     Join the community on <a href="https://www.facebook.com/James-Burkes-Knowledge-Web-1636642569892667/">Facebook</a>.<br>
     Want to work on the code? Contact karl@passionismandatory.com.
   </p>`)
 })
 
-app.use('/ui', express.static('./ui'))
+app.use('/', express.static('./ui'))
 
 // TODO move to separate file
 const api = express.Router()
